a8b4a5785dcd9f25c86bf8b21671062a019429c0
Caleb Jones <bionicle.dragon@gmail.com>
2014-10-28 21:54:00 -0400

    Initial commit
    Add script
    Add test script

---
+++ point_stream.py
@@ -0,0 +1,91 @@
+import pyglet
+from pyglet.gl import *
+import collections
+from threading import Thread
+import time
+import sys
+
+queue = collections.deque()
+
+header = sys.stdin.readline().split(" ")
+if len(header) < 1:
+    sys.stderr.write("The first line of must start with either the string '2d' or '3d'\n")
+    sys.stderr.flush()
+    sys.exit(1)
+
+header = [item.strip() for item in header]
+if header[0] == "2d":
+    num_dimensions = 2
+elif header[0] == "3d":
+    num_dimensions = 3
+else:
+    sys.stderr.write("The first line of must start with either the string '2d' or '3d'\n")
+    sys.stderr.flush()
+    sys.exit(1)
+
+hw, hh = 1, 1
+if len(header) >= 3:
+    hw = float(header[1])
+    hh = float(header[2])
+
+window = pyglet.window.Window()
+#event_loop = pyglet.app.EventLoop()
+
+points = []
+
+@window.event
+def on_draw():
+    global points
+
+    glClear(GL_COLOR_BUFFER_BIT)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-hw, hw, -hh, hh, -1, 1)
+    glMatrixMode(GL_MODELVIEW)
+    glPointSize(5)
+    
+    glLoadIdentity()
+    try:
+        points = queue.pop()
+    except IndexError:
+        pass
+    if points == "done":
+        sys.exit(0)
+
+    glBegin(GL_POINTS)
+    for p in points:
+        glVertex2f(p[0], p[1])
+    glEnd()
+    
+def populate_queue(queue):
+    while True:
+        line = sys.stdin.readline().strip()
+        if line == "done" or line == "\0":
+            queue.append("done")
+            sys.exit(0)
+
+        line = line.split()
+        points = []
+        for i in range(0, len(line), 2):
+            point = (float(line[i]),
+                     float(line[i+1]))
+            points.append(point)
+
+        queue.clear()
+        queue.append(points)
+    
+
+worker = Thread(target=populate_queue, args=(queue,))
+
+worker.setDaemon(True)
+worker.start()
+
+while True:
+    pyglet.clock.tick()
+
+    for window in pyglet.app.windows:
+        window.switch_to()
+        window.dispatch_events()
+        window.dispatch_event('on_draw')
+        window.flip()
+    time.sleep(1.0/120)
